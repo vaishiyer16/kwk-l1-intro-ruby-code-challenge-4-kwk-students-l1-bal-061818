db1712b07ac09048878b2f6e68fa166d81b67253
vaishiyer16 <vaishnavi.l.iyer@gmail.com>
2018-06-21 16:05:19 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -8,19 +8,19 @@ least_coins = {
   "pennies" => 0
 }
   while cents >= 25
-  least_coins["quarters"] += 1
+  least_coins[:quarters] += 1
   cents -= 25
   end
   while cents >= 10
-    least_coins["dime"] += 1
+    least_coins[:dime] += 1
     cents -= 10
     end
   while cents >= 5
-    least_coins["nickels"] += 1
+    least_coins[:nickels] += 1
     cents -= 5
   end
   while cents >= 1
-    least_coins["pennies"] += 1
+    least_coins[:pennies] += 1
     cents -= 1
   end
   puts least_coins
